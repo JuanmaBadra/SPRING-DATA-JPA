09bfb0930bdbaa75acc07073cbc45a1cd0980845
Juan Manuel Badra <85088551+JuanmaBadra@users.noreply.github.com>
2023-12-08 09:56:39 -0300

    Delete Desktop/CRUD'OnyToMany/hotel directory
    Elimino carpeta

--- Desktop/CRUD'OnyToMany/hotel/src/main/java/com/example/hotel/repository/HuespedRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.hotel.repository;
-
-import com.example.hotel.model.Huesped;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HuespedRepository extends JpaRepository<Huesped,Long> {
-
-}
